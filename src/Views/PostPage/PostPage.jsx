a4b089f305e86a75d3937a07d5467e0668cff133
Denis Kravchuk <denis.kravchuk.ua@gmail.com>
2021-12-16 01:32:21 +0200

    added PostPages views

---
+++ src/Views/PostPage/PostPage.jsx
@@ -0,0 +1,38 @@
+import styles from './PostPage.module.css';
+
+export function PostPage(params) {
+  return (
+    <>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet soluta
+        hic excepturi quisquam deserunt, sapiente rerum placeat iste, voluptatum
+        modi perferendis, est culpa minima dicta debitis quasi sit cupiditate
+        aspernatur.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet soluta
+        hic excepturi quisquam deserunt, sapiente rerum placeat iste, voluptatum
+        modi perferendis, est culpa minima dicta debitis quasi sit cupiditate
+        aspernatur.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet soluta
+        hic excepturi quisquam deserunt, sapiente rerum placeat iste, voluptatum
+        modi perferendis, est culpa minima dicta debitis quasi sit cupiditate
+        aspernatur.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet soluta
+        hic excepturi quisquam deserunt, sapiente rerum placeat iste, voluptatum
+        modi perferendis, est culpa minima dicta debitis quasi sit cupiditate
+        aspernatur.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet soluta
+        hic excepturi quisquam deserunt, sapiente rerum placeat iste, voluptatum
+        modi perferendis, est culpa minima dicta debitis quasi sit cupiditate
+        aspernatur.
+      </p>
+    </>
+  );
+}
